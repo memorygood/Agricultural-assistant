e1be60283d832436d6dbc6d57a6f153b5a73f01e
memorygood <2283653335@qq.com>
2018-03-10 17:32:51 +0800

    jiyi
    launch&main

---
+++ app/src/main/java/com/njau/agricultural_assistant/LaunchActivity.java
@@ -0,0 +1,48 @@
+package com.njau.agricultural_assistant;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.Indicators.PagerIndicator;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jiyi on 2018/3/1.
+ */
+
+public class LaunchActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //加载启动图片
+        setContentView(R.layout.activity_launch);
+        Integer time = 2000;    //设置等待时间，单位为毫秒
+        Handler handler = new Handler();
+        //当计时结束时，跳转至主界面
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LaunchActivity.this, MainActivity.class));
+                LaunchActivity.this.finish();
+            }
+        }, time);
+    }
+}
