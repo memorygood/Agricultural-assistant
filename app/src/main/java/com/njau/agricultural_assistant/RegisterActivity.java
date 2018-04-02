e72d6debc1b5979d23cdd03274e3f31ddfe9e461
memorygood <2283653335@qq.com>
2018-04-03 01:51:45 +0800

    memory
    register ui

--- app/src/main/java/com/njau/agricultural_assistant/RegisterActivity.java
+++ app/src/main/java/com/njau/agricultural_assistant/RegisterActivity.java
@@ -1,9 +1,11 @@
 package com.njau.agricultural_assistant;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hjm.bottomtabbar.BottomTabBar;
@@ -20,6 +22,17 @@ public class RegisterActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_register);
          Topbar topbar = (Topbar) findViewById(R.id.topbar);
+        Typeface font = Typeface.createFromAsset(getAssets(), "font/fontawesome-webfont.ttf");
+        TextView tv_usrname = (TextView)findViewById(R.id.iv_userIconName);
+        TextView tv_pwd = (TextView)findViewById(R.id.iv_userIconPwd);
+        TextView tv_checkpwd = (TextView)findViewById(R.id.iv_checkIconPwd);
+        TextView tv_email = (TextView)findViewById(R.id.iv_email);
+        TextView tv_phone = (TextView)findViewById(R.id.iv_phone);
+        tv_usrname.setTypeface(font);
+        tv_pwd.setTypeface(font);
+        tv_checkpwd.setTypeface(font);
+        tv_phone.setTypeface(font);
+        tv_email.setTypeface(font);
         topbar.setRightButtonVisibility(false);
         topbar.setOnLeftAndRightClickListener(new Topbar.OnLeftAndRightClickListener() {
             @Override
